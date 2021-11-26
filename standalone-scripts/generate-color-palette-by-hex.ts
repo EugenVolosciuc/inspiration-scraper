d23f703b4138611dc85cffa28af83d78a3b2ff0d
EugenVolosciuc <volosciuc.eugen@gmail.com>
2021-11-26 11:36:32 +0200

    Added standalone scripts for palette generation

---
+++ standalone-scripts/generate-color-palette-by-hex.ts
@@ -0,0 +1,47 @@
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import fs from "fs-extra";
+import { registerFont } from "canvas";
+import colorName from "color-namer";
+
+import {
+  getColorSchemeFromImage,
+  generateColorSchemeImage,
+} from "../utils/article";
+import writeToConsole from "../utils/writeToConsole";
+import { Color } from "../types/Color";
+
+require("dotenv").config();
+
+// file title, hexCodes[]
+/**
+ * @example
+ * // npm run generate-palette:hex -- --codes "#4EABD1" "#25272C" --title=Gemini
+ */
+const main = async () => {
+  registerFont("assets/fonts/WorkSans-Regular.ttf", { family: "Work Sans" });
+
+  const { title, codes, ...rest } = await yargs(hideBin(process.argv)).array(
+    "codes"
+  ).argv;
+
+  console.log("rest", rest);
+
+  console.log("codes", codes);
+  if (!title) throw new Error("Please provide a title argument");
+  if ((codes as string[])?.length < 1)
+    throw new Error("Please provide an array of hex codes");
+
+  const colors: Color[] = [];
+  for (let code of codes as string[]) {
+    const { hex, name } = colorName(code).ntc[0];
+
+    colors.push({ hex, name });
+  }
+
+  await generateColorSchemeImage(colors, title as string);
+
+  writeToConsole("Palette generated successfully");
+};
+
+main();
---
+++ standalone-scripts/generate-color-palette-by-image-title.ts
@@ -0,0 +1,42 @@
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import fs from "fs-extra";
+import { registerFont } from "canvas";
+
+import {
+  getColorSchemeFromImage,
+  generateColorSchemeImage,
+} from "../utils/article";
+import writeToConsole from "../utils/writeToConsole";
+
+require("dotenv").config();
+
+/**
+ * @example
+ * // npm run generate-palette:image -- --title=Gemini --numberOfColors=3
+ */
+const main = async () => {
+  registerFont("assets/fonts/WorkSans-Regular.ttf", { family: "Work Sans" });
+
+  try {
+    const { title, numberOfColors } = await yargs(hideBin(process.argv)).argv;
+    if (!title) throw new Error("Please provide a title argument");
+
+    const screenshot = await fs.readFile(`./assets/screenshots/${title}.png`);
+
+    const colors = await getColorSchemeFromImage(
+      screenshot,
+      (numberOfColors as number) ||
+        parseInt(process.env.COLORS_IN_PALETTE as string, 10)
+    );
+
+    await generateColorSchemeImage(colors, title as string);
+
+    writeToConsole("Palette generated successfully");
+  } catch (error) {
+    console.error(error);
+    writeToConsole("Could not generate color palette");
+  }
+};
+
+main();
